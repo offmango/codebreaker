2f45c31079299569ae9070804829a51f0b1e37dd
Eric Kurzenberger <offmango@gmail.com>
2012-08-10 15:31:17 -0400

    added another spec and executable

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -2,11 +2,12 @@ module Codebreaker
 	class Game
 		
 		def initialize(output)
-		
+			@output = output
 		end
 
 		def start
-		
+			@output.puts 'Welcome to Codebreaker!'	
+			@output.puts 'Enter guess:'
 		end
 	end
 end
