44c90e31d4808585a0ae92b0e1d6b722dda95288
offmango <offmango@gmail.com>
2012-08-15 22:24:16 -0400

    up to section 7.3 p91

--- spec/codebreaker/game_spec.rb
+++ spec/codebreaker/game_spec.rb
@@ -25,6 +25,27 @@ module Codebreaker
 					game.guess('5555')
 				end
 			end
+            context "with 1 number match" do
+                it "sends a mark with '-'" do
+                    game.start('1234')
+                    output.should_receive(:puts).with('-')
+                    game.guess('2555')
+                end
+            end
+            context "with 1 exact match" do
+                it "sends a mark with '+'" do
+                    game.start('1234')
+                    output.should_receive(:puts).with('+')
+                    game.guess('1555')
+                end
+            end
+            context "with 2 number matches" do
+                it "sends a mark with '--'" do
+                    game.start('1234')
+                    output.should_receive(:puts).with('--')
+                    game.guess('2355')
+                end
+            end
 		end
 	end
 end
